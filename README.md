6e83fb546ebc6798a7b3e6eb8cd3550a4237bd5d
BurakBugraAkar <burakbugraakar@gmail.com>
2021-10-27 14:59:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center">
+  <img src="https://img.shields.io/maintenance/yes/2021?style=for-the-badge" alt="Maintained">
+  <a href="https://www.npmjs.com/package/burak"><img src="img.shields.io/npm/v/burak.svg?sanitize=true&style=for-the-badge" alt="Version"></a>
+  <a href="https://github.com/BurakBugraAkar/burak/blob/dev/LICENSE"><img src="https://img.shields.io/npm/l/routve.svg?sanitize=true&style=for-the-badge" alt="License"></a>
+</p>
---
+++ burak.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+"use strict";
+
+// Clear Terminal
+process.stdout.write("\u001b[3J\u001b[2J\u001b[1J\u001b[3J");
+console.clear();
+
+const config          =   require("./burak.json");
+
+const terminalImage   =   require('terminal-image');
+const chalk           =   require('chalk');
+const got             =   require('got');
+const ww              =   require('word-wrap');
+
+const inquirer        =   require('inquirer');
+const open            =   require('open');
+
+function openSocialSelector() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'For other things...',
+      name: 'link',
+      choices: [
+        { name: chalk.bold.hex('#888888')(`💻  GitHub`), value: 'https://github.com/BurakBugraAkar' },
+        { name: chalk.bold.hex('#1DA1F2')(`❤️  Npm`), value: 'https://www.npmjs.com/~burakbugraakar' },
+        { name: chalk.bold.hex('#f04a45')('👋  Nope. Bye.'), value: false }
+      ]
+    }
+  ])
+  .then((result) => {
+    if (result.link) {
+      open(result.link);
+      openSocialSelector();
+    } else {
+      process.exit();
+    }
+  })
+  .catch(() => {});
+}
+
+console.log("")
+got(config.avatar, { responseType: 'buffer' })
+  .then((image) => terminalImage.buffer(image.body, { width: '26%' }))
+  .then((image) => image.split("\n").forEach((line) => console.log("  " + line)))
+  .then(() => {
+    console.log(ww(eval(`chalk\`${config.title}\``), { width: 200, trim: true }))
+  })
+  .then(() => {
+    console.log("\n")
+    openSocialSelector();
+  })
+  .catch((e) => console.log(e));
